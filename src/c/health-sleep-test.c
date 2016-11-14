a4822c90346f7291d9216b453e2fb8986404fa95
David Morgan <dmorgan81@gmail.com>
2016-11-14 09:42:10 -0500

    Console layer

--- src/c/health-sleep-test.c
+++ src/c/health-sleep-test.c
@@ -1,15 +1,34 @@
 #include <pebble.h>
+#include <pebble-console-layer/console-layer.h>
+
+const char *bit_rep[16] = {
+    [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
+    [ 4] = "0100", [ 5] = "0101", [ 6] = "0110", [ 7] = "0111",
+    [ 8] = "1000", [ 9] = "1001", [10] = "1010", [11] = "1011",
+    [12] = "1100", [13] = "1101", [14] = "1110", [15] = "1111",
+};
 
 static Window *s_window;
 static TextLayer *s_text_layer;
+static Layer *s_console_layer;
 
 static void health_handler(HealthEventType event, void *context) {
     if (event == HealthEventSignificantUpdate) {
         health_handler(HealthEventSleepUpdate, context);
-    } else if (event == HealthEventSleepUpdate) {
+    } else if (event != HealthEventHeartRateUpdate) {
+        time_t now = time(NULL);
+        struct tm *tick_time = localtime(&now);
+        char stime[16];
+        strftime(stime, sizeof(stime), "%m-%d %H:%I:%S", tick_time);
+
         HealthActivityMask mask = health_service_peek_current_activities();
-        bool sleeping = (mask & HealthActivitySleep) || (mask & HealthActivityRestfulSleep);
-        text_layer_set_text(context, sleeping ? "You are sleeping" : "You are awake");
+        char s[32];
+        snprintf(s, sizeof(s), "%s_%d_%s", stime, event, bit_rep[mask & 0x0F]);
+        console_layer_write_text(s_console_layer, s);
+        if (event == HealthEventSleepUpdate) {
+            bool sleeping = (mask & HealthActivitySleep) || (mask & HealthActivityRestfulSleep);
+            text_layer_set_text(context, sleeping ? "Asleep" : "Awake");
+        }
     }
 }
 
@@ -18,8 +37,11 @@ static void window_load(Window *window) {
     GRect bounds = layer_get_bounds(root_layer);
 
     s_text_layer = text_layer_create(bounds);
-    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
+    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     layer_add_child(root_layer, text_layer_get_layer(s_text_layer));
+
+    s_console_layer = console_layer_create(GRect(bounds.origin.x, bounds.origin.y + 20, bounds.size.w, bounds.size.h - 20));
+    layer_add_child(root_layer, s_console_layer);
 
     health_handler(HealthEventSignificantUpdate, s_text_layer);
     health_service_events_subscribe(health_handler, s_text_layer);
@@ -27,6 +49,7 @@ static void window_load(Window *window) {
 
 static void window_unload(Window *window) {
     health_service_events_unsubscribe();
+    console_layer_destroy(s_console_layer);
     text_layer_destroy(s_text_layer);
 }
 
